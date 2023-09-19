9eac242768a976d8dd7851e51611c3bcc203dcc3
Kiran Surya <kiranpowertracks724@gmail.com>
2023-09-19 17:32:00 +0530

    Links are added with hover effects

--- javascript/script.js
+++ javascript/script.js
@@ -2,12 +2,22 @@
 
 let btn = document.querySelector(".btn-menu");
 
+let navLinks = document.querySelectorAll(".main-link");
+
 let header = document.querySelector("header");
 
+// Opens nav bar
 btn.addEventListener("click",()=>{
     header.classList.toggle("trigger");
 })
 
+// When a link is clicked nav bar is closed
+navLinks.forEach((val)=>{
+    val.addEventListener("click",()=>{
+        header.classList.remove("trigger");
+    })
+    
+})
 // Brand Name Scroll
 
 
